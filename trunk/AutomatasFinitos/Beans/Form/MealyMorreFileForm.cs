cdee70990942813b064abde3f1e96bfc4efad2bf
njserranor@gmail.com <njserranor@gmail.com@c59e4076-7fcf-846f-f7e6-d31ec8409947>
2014-06-18 02:10:47 +0000

    - Pantalla acerca de - Generación de archivo con definiciones de Mealy - Corrección de errores en validaciones en MealyImplement.cs y PDAImplement.cs

---
+++ trunk/AutomatasFinitos/Beans/Form/MealyMorreFileForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AutomatasFinitos.Form
+{
+    class MealyMorreFileForm
+    {
+        public string estados { get; set; }
+        public string alfabetoEntrada { get; set; }
+        public string alfabetosalida { get; set; }
+        public string estadoinicial { get; set; }
+        public string funcionesTransición { get; set; }
+        public string funcionesSalida { get; set; }
+    }
+}
