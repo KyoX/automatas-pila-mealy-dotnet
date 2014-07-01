0bba7393a199cf1ff895f772e75864faba549b81
njserranor@gmail.com <njserranor@gmail.com@c59e4076-7fcf-846f-f7e6-d31ec8409947>
2014-07-01 04:34:16 +0000

    genera las transiciones de los automatas de moore

--- trunk/AutomatasFinitos/Implement/MooreImplement.cs
+++ trunk/AutomatasFinitos/Implement/MooreImplement.cs
@@ -9,11 +9,58 @@ namespace AutomatasFinitos.Implement
     class MooreImplement
     {
         private Mealy_Moore mm;
+        public int delay { get; set; }
+        public string stack { get; set;}
+        public string lastState { get; set; } // estado en que ha quedado el automata
 
-        public MooreImplement(Mealy_Moore moore) { this.mm = moore; }
+
+        public MooreImplement(Mealy_Moore moore) { 
+            this.mm = moore; 
+        }
 
         public MooreImplement() { }
+        /**
+         *
+         * valido solo la función de transición porque en caso de no existir no 
+         * se puede hacer nada más, y termina la ejecución. En caso de que no exista
+         * la función de salida, se generará la salida por defecto, la cual es: 
+         * & (cadena vacía) por eso no se valida
+         * 
+         **/
+        public bool validateTransition(string entrada) {
+            return mm.funcTransicion.ContainsKey(lastState+entrada); // 
+        }
 
+        public string generateTransition(string entrada)
+        {
+            string temp = mm.funcTransicion[lastState + entrada];
+            lastState = temp;                                   // actualizo al ultimo estado
+            return temp;
+        }
 
+        /**
+         * 
+         * Llamar a esta función antes de generateTransition, así no afecta su salida
+         * 
+         * */
+        public string generateOutputVal(string entrada)
+        {
+            if (mm.funcSalida.ContainsKey(lastState + entrada))
+            {
+                stack = mm.funcSalida[lastState + entrada];
+            }
+            else
+            {
+                stack = "&";
+            }
+            return stack;
+        }
+
+        internal void inicializar(double delay)
+        {
+            stack = "&";
+            lastState = mm.estadoInicial;
+            this.delay = (int)delay * 100;
+        }
     }
 }
--- trunk/AutomatasFinitos/MainWindow.xaml.cs
+++ trunk/AutomatasFinitos/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using AutomatasFinitos.Implement;
 using AutomatasFinitos.Pantallas;
+using System.Threading;
 
 namespace AutomatasFinitos
 {
@@ -54,6 +55,10 @@ namespace AutomatasFinitos
             string nombre = (string)source.Header;
             MooreGenerator mG = new MooreGenerator();
             PDAGenerator pila = new PDAGenerator();
+
+            moore = null;
+            pda = null;
+
             if (nombre.Contains("Moore")) 
             { 
                 mG.ShowDialog();
@@ -79,14 +84,57 @@ namespace AutomatasFinitos
         // ejecuta la maquina de Moore o el automata de pila de acuerdo a la definición cargada
         private void button2_Click(object sender, RoutedEventArgs e)
         {
+            string[] cintaEntradas = this.textBox1.Text.Split(',');
+            this.textBox2.Text = "";
+            this.textBox3.Text = "";
 
+            string temp;
+            if (moore != null)  // se quiere que se ejecute un autómata de moore
+            {
+                moore.inicializar(this.slider1.Value);  // setea los valores necesarios para inicializar el automata de moore
+                foreach (string entrada in cintaEntradas)
+                {
+                    if (moore.validateTransition(entrada))
+                    {
+                        temp = moore.generateOutputVal(entrada);
+                        Console.WriteLine("Salida: " + temp);
+                        this.textBox3.Text = this.textBox3.Text + temp;
+
+                        temp = moore.generateTransition(entrada);
+                        Console.WriteLine("Transición: " + moore.lastState + "," + entrada
+                            + " -> " + temp);
+
+                        this.textBox2.Text = moore.stack;
+                    }
+                    else
+                    {
+                        MessageBox.Show("El autómata no esta definido correctamente\n"
+                            + "Falta la definición para δ(" + moore.lastState + "," + entrada + ")",
+                            "Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+                        break;
+                    }
+                }
+
+
+                // pongo lo que quedó en el stack
+                Console.WriteLine("Salida: " + moore.stack);
+                this.textBox2.Text = moore.stack;
+                this.textBox3.Text = this.textBox3.Text + moore.stack;
+            }
+            else
+            {
+
+            }
+            
         }
 
         // pone en blanco los campos y stack/salida, tambien la definición de los automatas
         private void button3_Click(object sender, RoutedEventArgs e)
         {
-            moore = new MooreImplement();
-            pda = new PDAImplement();
+            moore = null;
+            pda = null;
+            this.textBox1.Text = "";
+            this.label4.Content = "Tipo Automata : ---";
         }
 
 
